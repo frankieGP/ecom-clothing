e196f37cb3f9897fd8fe513982b5c19b86eef36f
Frankie Gavin <frankanthonygavin@gmail.com>
2019-11-21 18:17:12 -0800

    creates a routable  shop page that takes props of the collection-preview component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
-
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
 
 
-const HatsPage = () =>
-  <div>
-    <h1>HATS PAGE</h1> 
-  </div>
+
 function App() {
   return (
     <div>
-      <Route path='/' component={HomePage} />
-      <Route path='/hats' component={HatsPage} />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route  path='/shop' component={ShopPage} />
 
+      </Switch>
     </div>
   );
 }
