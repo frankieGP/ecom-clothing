4a2b7d6d936de0515461c03e04f695bb6c2314e4
Frankie Gavin <frankanthonygavin@gmail.com>
2019-12-17 12:45:11 -0800

    adds firebase to project and Google auth

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
+import { signInWithGoogle } from '../../firebase/firebase.utils';
+
+
 import './sign-in.styles.scss';
 
 //CLASS Component
@@ -52,6 +55,8 @@ class SignIn extends React.Component{
                     />
 
                     <CustomButton type='submit'> Sign In </CustomButton>
+                    <CustomButton onClick={signInWithGoogle}> {' '}Sign in with Google{' '} </CustomButton>
+
                 </form>
             </div>
          );
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,28 @@
+import firebase from 'firebase/app';
+
+import 'firebase/firestore';
+import 'firebase/auth';
+
+
+const config = {
+     apiKey: "AIzaSyCrFcJKgD7jJGZH-f10Oj_DbycoWjY3tOk",
+    authDomain: "ecom-db-bacf8.firebaseapp.com",
+    databaseURL: "https://ecom-db-bacf8.firebaseio.com",
+    projectId: "ecom-db-bacf8",
+    storageBucket: "ecom-db-bacf8.appspot.com",
+    messagingSenderId: "96611603926",
+    appId: "1:96611603926:web:5b660038c46baf3fcc4077",
+    measurementId: "G-V711LE996V"     
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+
+export default firebase;
