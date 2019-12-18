0b2d95e05a27ca2998ad1502be5daa3345d3cf42
Frankie Gavin <frankanthonygavin@gmail.com>
2019-12-18 13:15:38 -0800

    adds an unsub lifycycle method to auth

--- src/App.js
+++ src/App.js
@@ -15,13 +15,18 @@ class App extends React.Component {
     }
   }
 
+  unsubscribeFromAuth = null
+
   componentDidMount() {
-    auth.onAuthStateChanged(user => {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
       this.setState({ currentUser: user });
 
     });
   }
 
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
   render() {
     return (
       <div>
