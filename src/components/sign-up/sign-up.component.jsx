16a486f087e84c030c8135eeab6ed48a323aa168
Frankie Gavin <frankanthonygavin@gmail.com>
2019-12-19 13:14:36 -0800

    makes handleSubmit method async

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -79,7 +79,7 @@ class SignUp extends React.Component {
                     <FormInput
                         type='password'
                         name='password'
-                        value={password}
+                        value={this.state.password}
                         onChange={this.handleChange}
                         label='Password'
                         required
