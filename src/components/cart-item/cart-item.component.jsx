5b818eae7502ba621b3fbc26cd329beed581b68c
Frankie Gavin <frankanthonygavin@gmail.com>
2020-01-08 13:59:46 -0800

    adds checheckout page, checkout item component, and updated cart to use slectors from reselectors

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -4,12 +4,12 @@ import './cart-item.styles.scss';
 
 
 
-const CartItem = ({ item: { imageUrl, price, name, quanity} }) => (
+const CartItem = ({ item: { imageUrl, price, name, quantity} }) => (
     <div className='cart-item'>
         <img src={imageUrl} alt='item' />
         <div className='item-details'>
             <span className='name'>{name}</span>
-            <span className='price'>{quanity} x ${price}</span>
+            <span className='price'>{quantity} x ${price}</span>
         </div>
     </div>
 );
